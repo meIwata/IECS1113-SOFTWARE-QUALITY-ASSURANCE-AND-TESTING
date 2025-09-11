f3a344fd92e404dd4eb1f47ecdc8ba4a4e7445de
meiwata <waleilulu@gmail.com>
2025-09-11 12:43:10 +0800

    update

---
+++ src/main/java/org/example/BreakpointDemo.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class BreakpointDemo {
+    public static void main(String[] args) {
+        computeArea();
+    }
+
+    public static void computeArea() {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter diameter: ");
+//        int diameter = input.nextInt();
+
+        int diameter = 11;
+
+        double area = Math.PI * Math.pow(diameter / 2, 2);
+        double x = Math.PI * diameter;
+        System.out.println(diameter);
+        System.out.println(area);
+        System.out.println(x);
+    }
+}
