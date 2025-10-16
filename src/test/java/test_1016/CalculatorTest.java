0a1ff52a12ef28c48ac43499a2eab382315bd301
meiwata <waleilulu@gmail.com>
2025-10-16 10:52:43 +0800

    新增 CalculatorTest 類的測試初始化與清理方法，並改善加法測試的錯誤訊息

--- src/test/java/test_1016/CalculatorTest.java
+++ src/test/java/test_1016/CalculatorTest.java
@@ -1,12 +1,25 @@
 package test_1016;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    @BeforeEach
+    void init(){
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void tearDown(){
+        System.out.println("After Each");
+    }
+
     @Test
     void add() {
+
         Calculator calculator = new Calculator();
 //        System.out.println("pp");
 //        assertEquals(2, calculator.add(1, 2));
@@ -20,29 +33,30 @@ class CalculatorTest {
         assertAll("Test add()",
                 () -> {
                     System.out.println("pp");
-                    assertEquals(3, calculator.add(1, 2));
+                    assertEquals(3, calculator.add(1, 2), "add ERROR");
                 },
                 () -> {
                     System.out.println("pn");
-                    assertEquals(0, calculator.add(1, -1));
+                    assertEquals(0, calculator.add(1, -1), "add pn ERROR");
                 },
                 () -> {
                     System.out.println("np");
-                    assertEquals(0, calculator.add(-1, 1));
+                    assertEquals(0, calculator.add(-1, 1), "add np ERROR");
                 },
                 () -> {
                     System.out.println("nn");
-                    assertEquals(-2, calculator.add(-1, -1));
+                    assertEquals(-2, calculator.add(-1, -1), "add nn ERROR");
                 }
         );
     }
 
-//    @Test
-//    void add_pp() {
-//        Calculator calculator = new Calculator();
-//        int actual = calculator.add(1, 2);
-//        assertEquals(3, actual);
-//    }
+    @Test
+    void add_pp() {
+        Calculator calculator = new Calculator();
+        int actual = calculator.add(1, 2);
+        System.out.println("add_pp");
+        assertEquals(3, actual);
+    }
 //    @Test
 //    void add_pn() {
 //        Calculator calculator = new Calculator();
